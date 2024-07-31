df2fffc331a82148f0e53a9bbc63fd2a325094c9
Rounak Longani <65913155+ronzzz4@users.noreply.github.com>
2024-07-31 12:00:51 +0530

    Update Chatbox.js final?

--- src/Components/Chatbox.js
+++ src/Components/Chatbox.js
@@ -86,10 +86,10 @@ const escapeHtml = (unsafe) => {
 let isBotTyping = false;
 
 let ws
- ws = new WebSocket('wss://rosa-backend-7sr4m57mo-rounaklongani-gmailcoms-projects.vercel.app/ws');
+ ws = new WebSocket('wss://rosa-backend.vercel.app/ws');
 
 function connectWebSocket() {
-  ws = new WebSocket('wss://rosa-backend-7sr4m57mo-rounaklongani-gmailcoms-projects.vercel.app/ws');
+  ws = new WebSocket('wss://rosa-backend.vercel.app/ws');
 }
 // function formatTextResponse(response) {
 //   // Replace line breaks with <br> for HTML rendering
